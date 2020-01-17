8887860b81c368cfbadd673ff30819e498a4c26b
asanka-code <asanka.code@gmail.com>
2020-01-17 19:48:53 +0000

    Working on the login functionality.

--- emvidence-gui/main.py
+++ emvidence-gui/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
+import sys
 import io
 import random
 from flask import Response
@@ -9,11 +11,28 @@ from matplotlib.figure import Figure
 
 app = Flask(__name__)
 
+#-------------------------------------------------------------------------------
 @app.route("/")
 def index(name=None):
   #return render_template('index.html', name=name)
   return render_template('login.html', name=name)
 
+
+#-------------------------------------------------------------------------------
+@app.route("/user-authentication", methods=['POST', 'GET'])
+def authentication():
+  # print(request.method, file=sys.stderr)
+  # print(request.form['uname'] + " " + request.form['passwd'], file=sys.stderr)
+
+  if is_passwd_correct(request.form['uname'], request.form['passwd']):
+    # correct password
+    return render_template('dashboard.html')
+  else:
+    # incorrect password
+    return render_template('login.html')
+
+
+#-------------------------------------------------------------------------------
 @app.route("/plot")
 def plot():
   fig = create_figure()
@@ -21,6 +40,11 @@ def plot():
   FigureCanvas(fig).print_png(output)
   return Response(output.getvalue(), mimetype='image/png')
 
+
+################################### Functions ######################################
+
+
+#-------------------------------------------------------------------------------
 def create_figure():
   fig = Figure()
   axis = fig.add_subplot(1, 1, 1)
@@ -31,6 +55,18 @@ def create_figure():
   axis.set_ylabel("Amplitude")
   return fig
   #return "Analysis page!"
+
+
+#-------------------------------------------------------------------------------
+def is_passwd_correct(uname, passwd):
+  '''
+  This function takes a username and a password as parameters and lookup in the
+  database. Returns true if the username and passwords matches. Returns false otherwise.
+  '''
+  if uname == 'asanka' and passwd == 'emvidence':
+    return True
+  else:
+    return False
   
 if __name__ == "__main__":
   app.run()
